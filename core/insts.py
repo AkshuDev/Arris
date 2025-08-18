f1c09f743e06d9c83db3314eb3f4f9f71339d193
AkshuDev <148709451+AkshuDev@users.noreply.github.com>
2025-08-18 16:36:08 +0000

    First

--- core/insts.py
+++ core/insts.py
@@ -1,16 +1,38 @@
 def mov(currentOffset:int) -> int:
     """Move 1 byte forward"""
-    return currentOffset + 1
+    currentOffset += 1
+    return currentOffset
 
 def movn(currentOffset:int, n:int) -> int:
     """Move n bytes forward"""
-    return currentOffset + n
+    currentOffset += n
+    return currentOffset
 
 def movb(currentOffset:int) -> int:
     """Move 1 byte backwards"""
-    return currentOffset - 1
+    currentOffset -= 1
+    return currentOffset
 
 def movbn(currentOffset:int, n:int) -> int:
     """Move n bytes backwards"""
-    return currentOffset - n
+    currentOffset -= n
+    return currentOffset
 
+def set(currentOffset:int, val:int) -> int:
+    """Set Current offset to a value"""
+    currentOffset = val
+    return val
+
+def clr(currentOffset:int, memory:bytearray) -> None:
+    memory[currentOffset] = int(0).to_bytes(1, "little")
+
+def run(lang:str, code:str) -> None:
+    if lang == "python" or lang == "py":
+        exec(code)
+    else:
+        return
+
+def include(code:str, file:str) -> None:
+    with open(file, "r") as f:
+        code += "\n" + f.read()
+    
--- core/main.py
+++ core/main.py
@@ -1,1 +1,62 @@
 # Arris
+import os
+import sys
+
+import insts
+
+if (len(sys.argv) < 2):
+    print("Usage: [program] <file>")
+
+def getInst(line:str, curOff:int) -> str:
+    char = ""
+    res = ""
+
+    while not char == "|":
+        char = line[curOff]
+        if char == "|":
+            break
+        res += char
+    
+    return res
+
+def parseInst(inst:str, nextInst:str, curMemOff:int, mem:bytearray, code:str) -> None:
+    if inst == "mov":
+        insts.mov(curMemOff)
+    elif inst == "movn":
+        insts.movn(curMemOff, int(nextInst))
+    elif inst == "movb":
+        insts.movb(curMemOff)
+    elif inst == "movbn":
+        insts.movbn(curMemOff, int(nextInst))
+    elif inst == "set":
+        insts.set(curMemOff, int(nextInst))
+    elif inst == "clr":
+        insts.clr(curMemOff, mem)
+    elif inst == "__py:":
+        insts.run("python", nextInst)
+    elif inst == "@inc":
+        insts.include(code, nextInst)
+    else:
+        raise ValueError("Unknown Instruction:", inst)
+
+if __name__ == "__main__":
+    data = ""
+    offset = 0
+    memory = bytearray(512)
+    memoffset = 0
+
+    if (len(sys.argv) > 2):
+        memory = bytearray(int(sys.argv[2]))
+    
+    with open(sys.argv[1], "r") as f:
+        data = f.read()
+
+    lineNo = 0
+    lines = data.splitlines()
+
+    while True:
+        line = lines[lineNo]
+        inst = getInst(line, offset)
+        Ninst = getInst(line, offset + len(inst) + 1)
+
+        parseInst(inst, Ninst, memoffset, memory, data)
